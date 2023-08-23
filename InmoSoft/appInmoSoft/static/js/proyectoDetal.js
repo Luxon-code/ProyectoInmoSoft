699d725dbd11d346ff52ffbc80b8483972bcce31
Joseph Trujillo <jt9906157@gmail.com>
2023-08-23 16:35:31 -0500

    lo del carrusel hecho

--- InmoSoft/appInmoSoft/static/js/proyectoDetal.js
+++ InmoSoft/appInmoSoft/static/js/proyectoDetal.js
@@ -5,13 +5,18 @@ function readCarrusel(id){
         .then(data => {
             console.log(data);
             const fotosInmueble = document.getElementById('fotosInmueble')
+            const indicadores = document.getElementById('indicadores')
             let fotosInmuebles = ''
+            let indicadoresHTML = ''
             data.imagenes.forEach((imagen,index) => {
                 const inmuebleHtml = `<div class="carousel-item ${index==0?'active':''}" data-bs-interval="10000">
                 <img src="/media/${imagen.imagen}" class="d-block" style="width: 100%; height: 15rem;" >
               </div>`;
+                const indicadorHTML = `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${index}" class=${index==0 ?"active":""} ${index==0?'aria-current="true"':null} aria-label="Slide ${index+1}"></button>`
+                indicadoresHTML += indicadorHTML
                 fotosInmuebles += inmuebleHtml;
             });
+            indicadores.innerHTML = indicadoresHTML;
             fotosInmueble.innerHTML = fotosInmuebles;
         })
         .catch(error => console.error('Error fetching data:', error));
